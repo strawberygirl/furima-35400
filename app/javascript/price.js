5b41898ca4c19db02c0e75299ce426880a8638c9
strawberygirl <81805249+strawberygirl@users.noreply.github.com>
2021-05-14 21:29:37 +0900

    出品に必要な入力情報を完備

---
+++ app/javascript/price.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+  })
+});
+
+
+//item-price
+
+
+
+//window.addEventListener('load', () => {
+//  const priceInput = document.getElementById("item-price");
+//  console.log(priceInput);
+//});
