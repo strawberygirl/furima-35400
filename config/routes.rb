68586866025297a514a1777b99244f89f4d33ffc
strawberygirl <81805249+strawberygirl@users.noreply.github.com>
2021-05-19 19:06:55 +0900

    購入画面へのリンクを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
-  resources :orders, onyl: [:create]
+  resources :items do
+    resources :orders, onyl: [:index, :create]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_012350) do
+ActiveRecord::Schema.define(version: 2021_05_19_092542) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,11 @@ ActiveRecord::Schema.define(version: 2021_05_18_012350) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "sold_users_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
