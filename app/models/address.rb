7d7df0acc064ef1428c550ad751642c827e69c3f
strawberygirl <81805249+strawberygirl@users.noreply.github.com>
2021-05-21 06:21:37 +0900

    validates の修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,14 +4,4 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
-  #with_options presence: true do
-  #  validates :order_id
-  #  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-  #  prefecture_id = prefecture_id
-  #  validates :prefecture_id, numericality: { other_than: 1 }
-  #  validates :city
-  #  validates :address
-  #  validates :phone_number
-  #end
-  #validates :building_name
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,13 +6,12 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :token
-    validates :postal_code,      format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :postal_code,   format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
     validates :address
-    validates :phone_number
+    validates :phone_number,  format: { with: /\A\d{11}\z/ }
   end
-  #validate :building_name
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
